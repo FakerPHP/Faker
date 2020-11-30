a6f8f0adb74bbcb9838acc370d88bf051d27d780
Daniel Schmelz <daniel@schmelz.org>
2020-11-30 13:06:06 +0100

    improve austrian social security number generation
    Signed-off-by: Daniel Schmelz <daniel@schmelz.org>

--- test/Faker/Provider/de_AT/PersonTest.php
+++ test/Faker/Provider/de_AT/PersonTest.php
@@ -22,7 +22,7 @@ final class PersonTest extends TestCase
         $this->faker = $faker;
     }
 
-    public function testSsn()
+    public function testSsnWithDefaultValuesCorrect()
     {
         for ($i = 0; $i < 100; $i++) {
             $number = $this->faker->ssn;
@@ -35,5 +35,21 @@ final class PersonTest extends TestCase
             $this->assertGreaterThanOrEqual(100, $consecutiveNumber);
             $this->assertLessThanOrEqual(999, $consecutiveNumber);
         }
+    }
+
+    public function testSsnWithGivenBirthDateCorrect()
+    {
+        $number = $this->faker->ssn(new \DateTime('2010-09-08 06:05:04'));
+
+        $this->assertEquals('080910', substr($number, 4, 6));
+
+        // Should have 10 characters: 3 consecutive number, 1 verification number, 6 birth date
+        $this->assertMatchesRegularExpression('/^[1-9]{1}[0-9]{9}$/', $number, '');
+
+        // consecutive number must be between 100 and 999
+        $consecutiveNumber = substr($number, 0, 3);
+        $this->assertGreaterThanOrEqual(100, $consecutiveNumber);
+        $this->assertLessThanOrEqual(999, $consecutiveNumber);
+
     }
 }
