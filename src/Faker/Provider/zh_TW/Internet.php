a345a9dfc76c51df61fda1cfd73b15ff1671c8d7
Daniel Schmelz <daniel@schmelz.org>
2020-12-08 14:08:53 +0100

    deprecate zh_TW methods that only call base methods

--- src/Faker/Provider/zh_TW/Internet.php
+++ src/Faker/Provider/zh_TW/Internet.php
@@ -2,15 +2,33 @@
 
 namespace Faker\Provider\zh_TW;
 
+/**
+ * @deprecated Use {@link \Faker\Provider\Internet} instead
+ * @see \Faker\Provider\Internet
+ */
 class Internet extends \Faker\Provider\Internet
 {
+    /**
+     * @return string
+     * @throws \Exception
+     *
+     * @deprecated Use {@link \Faker\Provider\Internet::userName()} instead
+     * @see \Faker\Provider\Internet::userName()
+     */
     public function userName()
     {
-        return \Faker\Factory::create('en_US')->userName();
+        return parent::userName();
     }
 
+    /**
+     * @return string
+     * @throws \Exception
+     *
+     * @deprecated Use {@link \Faker\Provider\Internet::domainWord()} instead
+     * @see \Faker\Provider\Internet::domainWord()
+     */
     public function domainWord()
     {
-        return \Faker\Factory::create('en_US')->domainWord();
+        return parent::domainWord();
     }
 }
