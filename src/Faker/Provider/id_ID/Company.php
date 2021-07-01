d092bc5283c1ef794d6ec1859c3af9fcbe4790d6
Ciki Momogi <74030149+CikiMomogi@users.noreply.github.com>
2021-07-01 16:50:42 +0000

    Sort the job list

--- src/Faker/Provider/id_ID/Company.php
+++ src/Faker/Provider/id_ID/Company.php
@@ -18,12 +18,23 @@ class Company extends \Faker\Provider\Company
 
     /**
      * @see https://www.amesbostonhotel.com/macam-macam-profesi-pekerjaan/
+     * @see https://dukcapil.kedirikab.go.id/artikel/Jenis-Pekerjaan_ID10.html
      */
     protected static $jobTitleFormat = [
-        'Guru', 'Dosen', 'Polisi', 'Tentara', 'Pilot', 'Pramugari', 'Satpam', 'Nelayan', 'Penyelam', 'Nahkoda',
-        'Sopir', 'Kondektur', 'Masinis', 'Perawat', 'Dokter', 'Bidan', 'Pengacara', 'Programmer', 'Pedagang', 'Pemandu wisata',
-        'Penambang', 'Petani', 'Peternak', 'Model fashion', 'Designer', 'Tukang', 'Chef', 'Pramusaji', 'Kasir', 'Wartawan',
-        'Seniman', 'Penari', 'Penulis', 'Arsitek', 'Atlet', 'Profesional', 'Porter', 'Apoteker', 'Hakim', 'Jaksa', 'Montir',
+        'Akuntan', 'Apoteker', 'Arsitek', 'Atlet', 'Belum / Tidak Bekerja', 'Biarawati', 'Bidan', 'Buruh Harian Lepas',
+        'Buruh Nelayan / Perikanan', 'Buruh Peternakan', 'Buruh Tani / Perkebunan', 'Desainer', 'Dokter', 'Dosen',
+        'Guru', 'Hakim', 'Imam Masjid', 'Industri', 'Jaksa', 'Juru Masak', 'Karyawan BUMD', 'Karyawan BUMN',
+        'Karyawan Honorer', 'Karyawan Swasta', 'Kepala Desa', 'Kepolisian RI (POLRI)', 'Kondektur', 'Konstruksi',
+        'Konsultan', 'Masinis', 'Mekanik', 'Mengurus Rumah Tangga', 'Montir', 'Nahkoda', 'Nelayan / Perikanan',
+        'Notaris', 'Paraji', 'Pastor', 'Pedagang', 'Pegawai Negeri Sipil (PNS)', 'Pelajar / Mahasiswa', 'Pelaut',
+        'Pemandu Wisata', 'Pembantu Rumah Tangga', 'Penambang', 'Penata Busana', 'Penata Rambut', 'Penata Rias',
+        'Pendeta', 'Peneliti', 'Penerjemah', 'Pengacara', 'Pensiunan', 'Penulis', 'Penyelam', 'Penyiar Radio',
+        'Penyiar Televisi', 'Perancang Busana', 'Perangkat Desa', 'Perawat', 'Perdagangan', 'Petani / Pekebun',
+        'Peternak', 'Pialang', 'Pilot', 'Pramugari', 'Pramusaji', 'Presiden', 'Programmer', 'Promotor Acara',
+        'Psikiater / Psikolog', 'Satpam', 'Seniman', 'Sopir', 'Tabib', 'Tentara Nasional Indonesia (TNI)',
+        'Transportasi', 'Tukang Batu', 'Tukang Cukur', 'Tukang Gigi', 'Tukang Jahit', 'Tukang Kayu',
+        'Tukang Las / Pandai Besi', 'Tukang Listrik', 'Tukang Sol Sepatu', 'Ustaz / Mubaligh', 'Wakil Presiden',
+        'Wartawan', 'Wiraswasta',
     ];
 
     /**
