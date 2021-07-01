5cd612cede2902314d86ecc6dc516d9b76fe96cd
Ciki Momogi <74030149+CikiMomogi@users.noreply.github.com>
2021-07-01 15:10:49 +0000

    Add job list for localization id_ID
    Add job list for id_ID

--- src/Faker/Provider/id_ID/Company.php
+++ src/Faker/Provider/id_ID/Company.php
@@ -17,6 +17,16 @@ class Company extends \Faker\Provider\Company
     protected static $companyPrefix = ['PT', 'CV', 'UD', 'PD', 'Perum'];
 
     /**
+     * @see https://www.amesbostonhotel.com/macam-macam-profesi-pekerjaan/
+     */
+    protected static $jobTitleFormat = [
+        'Guru', 'Dosen', 'Polisi', 'Tentara', 'Pilot', 'Pramugari', 'Satpam', 'Nelayan', 'Penyelam', 'Nahkoda',
+        'Sopir', 'Kondektur', 'Masinis', 'Perawat', 'Dokter', 'Bidan', 'Pengacara', 'Programmer', 'Pedagang', 'Pemandu wisata',
+        'Penambang', 'Petani', 'Peternak', 'Model fashion', 'Designer', 'Tukang', 'Chef', 'Pramusaji', 'Kasir', 'Wartawan',
+        'Seniman', 'Penari', 'Penulis', 'Arsitek', 'Atlet', 'Profesional', 'Porter', 'Apoteker', 'Hakim', 'Jaksa', 'Montir',
+    ];
+
+    /**
      * @see http://id.wikipedia.org/wiki/Jenis_badan_usaha
      */
     protected static $companySuffix = ['(Persero) Tbk', 'Tbk'];
