05b6b50dd4f615368d37dc97498e7e6b3bcbbd4d
Madan Sapkota <code@madansapkota.com>
2021-09-15 00:03:00 +0545

    Compare the return values

--- test/Faker/Provider/ne_NP/PaymentTest.php
+++ test/Faker/Provider/ne_NP/PaymentTest.php
@@ -11,18 +11,34 @@ final class PaymentTest extends TestCase
     {
         $str = $this->faker->commercialBank();
         self::assertIsString($str);
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->commercialBank();
+        self::assertSame($str, $strNext);
     }
 
     public function testDevelopmentBank(): void
     {
         $str = $this->faker->developmentBank();
         self::assertIsString($str);
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->developmentBank();
+        self::assertSame($str, $strNext);
+        echo " $str -> $strNext";
     }
 
     public function testFinanceCompany(): void
     {
         $str = $this->faker->financeCompany();
         self::assertIsString($str);
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->financeCompany();
+        self::assertSame($str, $strNext);
     }
 
     public function testMicroFinance(): void
@@ -30,18 +46,33 @@ final class PaymentTest extends TestCase
         $str = $this->faker->microFinance();
         self::assertIsString($str);
         self::assertStringContainsString('Laghubitta', $str);
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->microFinance();
+        self::assertSame($str, $strNext);
     }
 
     public function testDigitalWallet(): void
     {
         $str = $this->faker->digitalWallet();
         self::assertIsString($str);
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->digitalWallet();
+        self::assertSame($str, $strNext);
     }
 
     public function testSwiftCode(): void
     {
         $str = $this->faker->swiftCode();
         self::assertIsString($str);
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->swiftCode();
+        self::assertSame($str, $strNext);
     }
 
     public function testBankAccountNumber(): void
@@ -50,6 +81,11 @@ final class PaymentTest extends TestCase
         self::assertIsString($str);
         self::assertGreaterThanOrEqual(9, strlen($str));
         self::assertLessThanOrEqual(20, strlen($str));
+
+        $this->faker->seed(1);
+
+        $strNext = $this->faker->bankAccountNumber();
+        self::assertSame($str, $strNext);
     }
 
     protected function getProviders(): iterable
