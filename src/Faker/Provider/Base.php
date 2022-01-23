540da623c6fbb87c3f69b5af80685d3fa640ec1b
Daniel Schmelz <daniel@schmelz.org>
2022-01-23 20:22:02 +0100

    TASK: fix regexify backslash in character class

--- src/Faker/Provider/Base.php
+++ src/Faker/Provider/Base.php
@@ -525,7 +525,7 @@ class Base
         }, $regex);
         // All [ABC] become B (or A or C)
         $regex = preg_replace_callback('/\[([^\]]+)\]/', static function ($matches) {
-            // remove single backslash
+            // remove backslashes (that are not followed by another backslash) because they are escape characters
             $matches = preg_replace('/\\\(?!\\\)/', '', $matches[1]);
             $randomElement = Base::randomElement(str_split($matches));
             //[.] should not be a random character, but a literal .
