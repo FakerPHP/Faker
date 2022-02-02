edefb8d62ac967f10ad2d0076cbef39adb0b2764
Andreas Frömer <andreas.froemer@check24.de>
2022-02-02 06:01:05 +0100

    Only load class alias when using Faker\ORM\Doctrine classes.
    This will also add autoload for phpstan to register the alias.

--- src/Faker/ORM/Doctrine/backward-compatibility.php
+++ src/Faker/ORM/Doctrine/backward-compatibility.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-if (class_exists('\Doctrine\Persistence\Mapping\ClassMetadata') && !class_exists('Doctrine\Common\Persistence\Mapping\ClassMetadata')) {
+if (!class_exists('Doctrine\Common\Persistence\Mapping\ClassMetadata')) {
     class_alias(\Doctrine\Persistence\Mapping\ClassMetadata::class, 'Doctrine\Common\Persistence\Mapping\ClassMetadata');
 }
 
-if (class_exists('\Doctrine\Persistence\ObjectManager') && !class_exists('Doctrine\Common\Persistence\ObjectManager')) {
+if (!class_exists('Doctrine\Common\Persistence\ObjectManager')) {
     class_alias(\Doctrine\Persistence\ObjectManager::class, 'Doctrine\Common\Persistence\ObjectManager');
 }
