79b751e2f84cff92fc63d758debb6549f9c61084
Robbin Janssen <robbinjanssen@gmail.com>
2022-04-11 10:46:01 +0200

    Rename msedge to edge

--- src/Faker/Provider/UserAgent.php
+++ src/Faker/Provider/UserAgent.php
@@ -88,7 +88,7 @@ class UserAgent extends Base
      *
      * @return string
      */
-    public static function msedge()
+    public static function edge()
     {
         $saf = self::numberBetween(531, 537) . '.' . self::numberBetween(0, 2);
         $chrv = self::numberBetween(79, 99) . '.0';
