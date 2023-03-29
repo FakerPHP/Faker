a1020fdedfee3b45b758052f8c76d04cf248c659
Glenn Mulleners <GlennM@users.noreply.github.com>
2023-03-29 10:26:20 +0200

    Dollar sign outside of curly braces is deprecated

--- src/Faker/Provider/cs_CZ/Person.php
+++ src/Faker/Provider/cs_CZ/Person.php
@@ -438,8 +438,8 @@ class Person extends \Faker\Provider\Person
             $gender = $this->generator->boolean() ? static::GENDER_MALE : static::GENDER_FEMALE;
         }
 
-        $startTimestamp = strtotime("-${maxAge} year");
-        $endTimestamp = strtotime("-${minAge} year");
+        $startTimestamp = strtotime("-{$maxAge} year");
+        $endTimestamp = strtotime("-{$minAge} year");
         $randTimestamp = self::numberBetween($startTimestamp, $endTimestamp);
 
         $year = intval(date('Y', $randTimestamp));
