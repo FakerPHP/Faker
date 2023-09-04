397a7d38670b4f01835fea0709faa6e38e316dd1
Andreas Möller <am@localheinz.com>
2023-09-04 21:31:48 +0200

    Fix: Simplify

--- src/Faker/Container/ContainerBuilder.php
+++ src/Faker/Container/ContainerBuilder.php
@@ -68,12 +68,6 @@ final class ContainerBuilder
 
     public static function getDefault(): ContainerInterface
     {
-        $instance = new self();
-
-        foreach (self::defaultExtensions() as $id => $definition) {
-            $instance->add($id, $definition);
-        }
-
-        return $instance->build();
+        return new Container(self::defaultExtensions());
     }
 }
