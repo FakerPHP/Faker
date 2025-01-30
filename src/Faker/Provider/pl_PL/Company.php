262ce38716eb2fe795ada8e953ce93a10b8ab853
Mateusz Lerczak <mateusz@lerczak.eu>
2025-01-30 12:39:17 +0100

    Add few popular job titles for pl_PL provider.

--- src/Faker/Provider/pl_PL/Company.php
+++ src/Faker/Provider/pl_PL/Company.php
@@ -19,6 +19,37 @@ class Company extends \Faker\Provider\Company
 
     protected static $companyPrefix = ['Grupa', 'Fundacja', 'Stowarzyszenie', 'Spółdzielnia'];
 
+    protected static $jobTitleFormat = [
+        // Administracja i biuro
+        'Archiwista', 'Asystent/ka dyrektora', 'Księgowy/ksęgowa', 'Recepcjonista/recepcjonistka', 'Sekretarka/sekreterz', 'Specjalista ds. administracji', 'Specjalista ds. kadr i płac',
+        // IT i technologie informacyjne
+        'Administrator systemów IT', 'Analityk danych', 'Inżynier DevOps', 'Kierownik projektów IT', 'Młodszy programista', 'Programista', 'Projektant stron internetowych', 'Specjalista ds. cyberbezpieczeństwa', 'Starszy programista', 'Tester oprogramowania',
+        // Finanse i bankowość
+        'Analityk finansowy', 'Doradca finansowy', 'Doradca kredytowy', 'Księgowy/ksęgowa', 'Makler giełdowy', 'Specjalista ds. BHP', 'Specjalista ds. controllingu', 'Specjalista ds. windykacji',
+        // Handel i sprzedaż
+        'Doradca klienta', 'Key Account Manager', 'Kierownik sklepu', 'Merchandiser', 'Przedstawiciel handlowy', 'Specjalista ds. marketingu', 'Specjalista ds. sprzedaży',
+        // Produkcja i przemysł
+        'Inżynier produkcji', 'Kierownik produkcji', 'Magazynier', 'Monter/monterka', 'Operator maszyn', 'Spawacz', 'Technolog produkcji',
+        // Budownictwo i nieruchomości
+        'Architekt', 'Deweloper', 'Geodeta', 'Inżynier budowy', 'Kierownik budowy', 'Monter instalacji sanitarnych', 'Specjalista ds. nieruchomości',
+        // Medycyna i opieka zdrowotna
+        'Asystent/ka stomatologiczna', 'Dietetyk', 'Farmaceuta', 'Fizjoterapeuta', 'Lekarz', 'Pielęgniarka/pielęgniarz', 'Ratownik medyczny',
+        // Edukacja i szkolenia
+        'Doradca zawodowy', 'Dyrektor szkoły', 'Korepetytor', 'Nauczyciel', 'Specjalista ds. szkoleń', 'Trener biznesu', 'Wychowawca przedszkolny',
+        // Transport i logistyka
+        'Dyspozytor', 'Kierowca', 'Kurier', 'Logistyk', 'Magazynier', 'Operator wózka widłowego', 'Specjalista ds. transportu',
+        // Hotelarstwo i gastronomia
+        'Barman/barmanka', 'Kelner/kelnerka', 'Kucharz/kucharka', 'Manager restauracji', 'Pracownik kuchni', 'Recepcjonista/recepcjonistka w hotelu', 'Specjalista ds. obsługi gości',
+        // Media i kreatywne zawody
+        'Copywriter', 'Dziennikarz', 'Fotograf', 'Grafik komputerowy', 'Producent filmowy', 'Redaktor', 'Specjalista ds. social media',
+        // Praca fizyczna i usługi
+        'Elektryk', 'Hydraulik', 'Mechanik samochodowy', 'Ogrodnik', 'Ochroniarz', 'Opiekun/ka osób starszych',
+        // Nauka i badania
+        'Biotechnolog', 'Chemik', 'Fizyk', 'Inżynier badawczo-rozwojowy', 'Laborant/laborantka', 'Naukowiec (różnych dziedzin)',
+        // Praca zdalna i freelancing
+        'Copywriter', 'Konsultant ds. e-commerce', 'Programista zdalny', 'Projektant graficzny', 'Specjalista ds. marketingu internetowego', 'Tłumacz/tłumaczka',
+    ];
+
     /**
      * @example 'Grupa'
      */
