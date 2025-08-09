d0c4ac1777dc6f5389e6b753971af1b722716c21
gncdev <gianegekck@gmail.com>
2025-08-09 19:58:34 +0300

    tests fix

--- src/Faker/Provider/Color.php
+++ src/Faker/Provider/Color.php
@@ -60,11 +60,13 @@ class Color extends Base
 
         return '#' . $color[0] . $color[0] . $color[1] . $color[1] . $color[2] . $color[2];
     }
+
     /**
      * Returns a suitable text color for the given background color.
      * If the background is dark, returns white (`FFFFFF`); if light, returns black (`000000`).
      *
      * @param string $backgroundHex Background color in hex format (e.g. '#CCCCCC')
+     *
      * @return string Text color hex code ('000000' or 'FFFFFF')
      */
     public static function safeTextColorForBackground($backgroundHex)
@@ -74,6 +76,7 @@ class Color extends Base
         $g = hexdec(substr($backgroundHex, 2, 2));
         $b = hexdec(substr($backgroundHex, 4, 2));
         $luminance = (0.299 * $r + 0.587 * $g + 0.114 * $b);
+
         return $luminance > 186 ? '000000' : 'FFFFFF';
     }
 
