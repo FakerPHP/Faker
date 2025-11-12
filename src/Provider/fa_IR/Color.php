c0dac21559323086f266043387aa12399fee1b8a
yoonustehrani <yoonustehrani28@gmail.com>
2025-11-12 12:08:05 +0330

    added safeColorNames and ref comment

--- src/Provider/fa_IR/Color.php
+++ src/Provider/fa_IR/Color.php
@@ -4,6 +4,16 @@ namespace Faker\Provider\fa_IR;
 
 class Color extends \Faker\Provider\Color
 {
+    protected static $safeColorNames = [
+        'سیاه', 'سفید', 'آبی', 'قرمز',
+        'زرد', 'سبز', 'نارنجی', 'بنفش',
+    ];
+    
+    /**
+     * A selection from the page below
+     *
+     * @see https://fa.wikipedia.org/wiki/%D9%81%D9%87%D8%B1%D8%B3%D8%AA_%D8%B1%D9%86%DA%AF%E2%80%8C%D9%87%D8%A7_(%D9%81%D8%B4%D8%B1%D8%AF%D9%87)
+     */
     protected static $allColorNames = [
         'لاجوردی', 'بژ', 'سیاه', 'آبی', 'قهوه‌ای', 'شکلاتی', 'زرشکی',
         'فیروزه‌ای', 'سرخابی', 'خاکستری', 'سفید', 'طلایی', 'سبز',
